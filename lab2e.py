b5eb665ecd10c7d7695d35c39cc81d1c0c29beb3
Agnestra Mahat <amahat1@myseneca.ca>
2025-06-05 13:53:45 -0400

    added lab1e

---
+++ lab2e.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#Author: Agnestra Mahat
+#Author ID: 12893938
+#Date Created: 2025/06/05
+
+# This line uses an integer object named timer with a value of 10 
+timer = 10
+
+# While loop that repeats itself until the bomb blasts
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+
+# kaboom
+print("blast off!")
+
+
+
+
