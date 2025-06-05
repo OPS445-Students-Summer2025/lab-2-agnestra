657ff04cf889e20a4402a56f20512dc0e8c318c0
Agnestra Mahat <amahat1@myseneca.ca>
2025-06-05 13:52:43 -0400

    added lab1a

---
+++ lab2b.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#This line prompts the user for name
+Name = input("Name: ")
+
+#This line prompts the user fo their age
+Age = input("Age: ")
+
+#This line prints Hi Name, you are Age years old.
+print('Hi ' + Name + ', you are ' + str(Age) + ' years old.')
+
+# The input value also prints the name and age value so the print statements for name and age were not needed.
+#This line prints the name 
+#print('Name: ' + Name)
+
+#This line prints the age
+#print('Age: ' + Age)
